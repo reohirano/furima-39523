a92478a520f8726cf42091549682e1c1a3b3e672
reohirano <137034337+reohirano@users.noreply.github.com>
2023-07-18 20:03:16 +0900

    テストコード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
   describe 'ユーザー新規登録' do
     it 'nicknameが空では登録できない' do
       @user.nickname = ''
@@ -23,19 +27,22 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
-    it 'nicknameが7文字以上では登録できない' do
-      
-    end
-    it '重複したemailが存在する場合は登録できない' do
-    end
     it 'emailは@を含まないと登録できない' do
+      @user.email = 'testmail'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Email is invalid')
     end
     it 'passwordが5文字以下では登録できない' do
+      @user.password = '00000'
+      @user.password_confirmation = '00000'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
     end
     it 'passwordが129文字以上では登録できない' do
+      @user.password = Faker::Internet.password(min_length: 129, max_length: 150)
+      @user.password_confirmation = @user.password
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is too long (maximum is 128 characters)")
     end
-
-
-  end
-  
+ end  
 end
