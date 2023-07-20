2eb7541eb98e726bf392fd95aac56662935b69a8
reohirano <137034337+reohirano@users.noreply.github.com>
2023-07-20 18:01:17 +0900

    出品した商品の保存

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,34 @@
 class Item < ApplicationRecord
-  has_one_attached :image
-  belongs_to :user
   extend ActiveHash::Associations::ActiveRecordExtensions
- 
+
+  belongs_to :user
+
   belongs_to :category
-  belongs_to :delivery_area
-  belongs_to :item_status
-  belongs_to :shipping_cost
-  belongs_to :shipping_date
-  
+  belongs_to :area
+  belongs_to :condition
+  belongs_to :cost
+  belongs_to :d_day
+
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :user_id
+    validates :image
+    validates :name
+    validates :explain
+    validates :category_id
+    validates :condition_id
+    validates :cost_id
+    validates :area_id
+    validates :d_day_id
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
+
+  with_options numericality: { other_than: 0 } do
+    validates :category_id
+    validates :area_id
+    validates :condition_id
+    validates :cost_id
+    validates :d_day_id
+  end
 end
