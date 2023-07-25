b5822065a6a91c8a4ec1134871a7a15fa1ed9ba2
reohirano <137034337+reohirano@users.noreply.github.com>
2023-07-25 18:27:20 +0900

    バリデーションの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,5 +4,20 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order_form = OrderForm.new(order_params)
+    if @order_form.valid?
+      pay_item
+      @order_form.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+
+  private
+
+  def order_params
+    params.require(:order_form).permit(:postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 end
--- db/migrate/20230724150146_create_sipping_addresses.rb
+++ db/migrate/20230724150146_create_sipping_addresses.rb
@@ -1,7 +1,13 @@
 class CreateSippingAddresses < ActiveRecord::Migration[7.0]
   def change
     create_table :sipping_addresses do |t|
-
+      t.references :order,         null: false, foreign_key: true
+      t.string     :postcode,      null: false
+      t.integer    :prefecture_id, null: false
+      t.string     :city,          null: false
+      t.string     :block,         null: false
+      t.string     :building
+      t.string     :phone_number,  null: false
       t.timestamps
     end
   end
