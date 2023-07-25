417707943d0a15d685fc8cc395c4dcedabf9ffa1
reohirano <137034337+reohirano@users.noreply.github.com>
2023-07-25 15:05:34 +0900

    ページ偏移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :show]
+  before_action :set_item, only: [:edit, :show, :update]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -20,10 +20,24 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    if @item.user_id == current_user.id
+    else
+      redirect_to root_path
+    end
   end
 
   def show
   end
+
+  def update
+    @item.update(item_params)
+    if @item.valid?
+      redirect_to item_path(item_params)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def item_params
