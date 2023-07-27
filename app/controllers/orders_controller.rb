6812b74f63979b1e379faf6c0a3c140c756edba2
reohirano <137034337+reohirano@users.noreply.github.com>
2023-07-27 12:26:25 +0900

    payJP導入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,7 @@ class OrdersController < ApplicationController
   def create
      @order_form = OrderForm.new(sipping_address_params)
    if @order_form.valid?
+     pay_item
      @order_form.save
      redirect_to root_path
    else
@@ -23,8 +24,12 @@ class OrdersController < ApplicationController
   def sipping_address_params
     params.require(:order_form).permit(:postcode, :area_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
-
-  #def order_params
-    #params.require(:order_form).permit(:user_id, :item_id)
-  #end
+  def pay_item
+   Payjp.api_key = "sk_test_022b55e313df35d3e582448a"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: order_params[:price],  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
+  end
 end
