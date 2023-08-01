7a8317675db91c8b7c66fa82c6bace4caa38f77d
reohirano <137034337+reohirano@users.noreply.github.com>
2023-08-01 16:35:31 +0900

    修正３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -40,6 +40,7 @@ class ItemsController < ApplicationController
   
 
   def destroy
+    @item = Item.find(params[:id])
     if @item.user_id == current_user.id
       @item.destroy
       redirect_to root_path
